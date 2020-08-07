7c95469abf53ef908c5512bd4f36a24fc1fb6527
linchaoyong <lincy18845091124@aliyun.com>
2020-08-07 16:01:38 +0800

    Initial commit

---
+++ src/main/java/pojo/Emp.java
@@ -0,0 +1,38 @@
+package pojo;
+
+public class Emp {
+    private String name;
+    private int age;
+
+    public Emp() {
+    }
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public Emp(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
